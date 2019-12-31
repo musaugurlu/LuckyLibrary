bb3b0e1d8d650c462aa3129197d7f08e6ab94e31
Musa Ugurlu <musaugurlu@outlook.com>
2019-12-30 20:49:09 -0500

    Front End adjustments

--- LuckyLibrary/src/main/resources/static/js/app.js
+++ LuckyLibrary/src/main/resources/static/js/app.js
@@ -1,159 +1,78 @@
-var App = new Vue({
-  el: "#app",
-  data() {
-    return {
-      search: "",
-      title: "Game of Thrones",
-      cards: [
-        {
-          name: "Season One",
-          cardColor: "dark",
-          items: [
-            "winter is coming",
-            "the kingsroad",
-            "lord snow",
-            "cripples, bastards and broken things",
-            "the wolf and the lion",
-            "a golden crown",
-            "you win or you die",
-            "the pointy end",
-            "baelor",
-            "fire and blood"
-          ]
-        },
-        {
-          name: "Season 2",
-          cardColor: "info",
-          items: [
-            "the north remembers",
-            "the night lands",
-            "what is dead may never die",
-            "garden of bones",
-            "the ghost of harrenhal",
-            "the old gods and the new",
-            "a man without honor",
-            "the prince of winterfell",
-            "blackwater",
-            "valar morghulis"
-          ]
-        },
-        {
-          name: "Season 3",
-          cardColor: "primary",
-          items: [
-            "valar dohaeris ",
-            "dark wings, dark words",
-            "walk of punishment",
-            "and now his watch is ended",
-            "kissed by fire",
-            "the climb",
-            "the bear and the maiden fair",
-            "second sons",
-            "the rains of castamere",
-            "mhysa"
-          ]
-        },
-        {
-          name: "Season 4",
-          cardColor: "info",
-          items: [
-            "two swords",
-            "the lion and the rose",
-            "breaker of chains",
-            "oathkeeper",
-            "first of his name",
-            "the laws of gods and men",
-            "mockingbird",
-            "the mountain and the viper",
-            "the watchers on the wall",
-            "the children"
-          ]
-        },
-        {
-          name: "Season 5",
-          cardColor: "success",
-          items: [
-            "the wars to come",
-            "the house of black and white",
-            "high sparrow",
-            "sons of the harpy",
-            "kill the boy",
-            "unbowed unbent unbroken",
-            "the gift",
-            "hardome",
-            "the dance of dragons",
-            "mother's mercy"
-          ]
-        },
-        {
-          name: "Season 6",
-          cardColor: "warning",
-          items: [
-            "the red woman",
-            "home",
-            "oathbreaker",
-            "book of the stranger ",
-            "the door",
-            "blood of my blood",
-            "the broken man",
-            "no one",
-            "battle of the bastards",
-            "the winds of winter"
-          ]
-        },
-        {
-          name: "Season 7",
-          cardColor: "danger",
-          items: [
-            "dragonstone",
-            "stormborn",
-            "the queen's justice",
-            "the spoils of war",
-            "eastwatch",
-            "beyond the wall",
-            "the dragon and the wolf"
-          ]
-        },
-        
-             {
-          name: "Season 8",
-          cardColor: "info",
-          items: [
-            "winterfell",
-            "a knight of the seven kingdoms",
-            "the long night",
-            "the last of the starks",
-            "the bells",
-            "the iron throne"
-          ]
-        }
-      ]     
-    }
-    },
+const bookCard = {
+    props: [
+        'imgSrc',
+        'title',
+        'subTitle',
+        'content'
+    ],
+    template: `
+    <div class="column is-one-quarter">
+        <div class="card">
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-left">
+                        <figure class="image is-128x128">
+                            <img :src="imgSrc" alt="Book">
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-4">{{title}}</p>
+                        <p class="subtitle is-6">{{subTitle}}</p>
+                    </div>
+                </div>
     
-      
-  computed: {
-    filteredCards() {
-      var _this = this
-      if(this.search.length === 0) return this.cards
-      return this.cards.map(function(card) {
-        return {
-          name: card.name,
-          cardColor: card.cardColor,
-          items: card.items.filter(function (item) {
-            return item.includes(_this.search)
-          })
-        }
-      })
-    }
-  },
-  
-  methods: {
-    removeCard (card) {
-      this.cards.splice(this.cards.indexOf(card), 1)
+                <div class="content">
+                    {{content}}
+                    <br>
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a href="#" class="card-footer-item">Save</a>
+                <a href="#" class="card-footer-item">Edit</a>
+                <a href="#" class="card-footer-item">Delete</a>
+            </footer>
+        </div>
+    </div>
+    `
+}
+
+var App = new Vue({
+    el: "#app",
+    components: {
+        'book-card' : bookCard
     },
-    clearSearchField () {
-      this.search = ''
+    data() {
+        return {
+            search: "",
+            title: "Books",
+            cards: []
+        }
+    },
+
+
+    computed: {
+        filteredCards() {
+            var _this = this
+            if(this.search.length === 0) return this.cards
+            return this.cards.map(function(card) {
+                return {
+                    name: card.name,
+                    cardColor: card.cardColor,
+                    items: card.items.filter(function (item) {
+                        return item.includes(_this.search)
+                    })
+                }
+            })
+        }
+    },
+
+    methods: {
+        removeCard (card) {
+            this.cards.splice(this.cards.indexOf(card), 1)
+        },
+        clearSearchField () {
+            this.search = ''
+        }
     }
-  }
 });
