1d6098771956e8c535e699d1ddff054bd2003128
Musa Ugurlu <musaugurlu@outlook.com>
2020-02-26 23:11:49 -0500

    Books

--- src/main/java/com/musaugurlu/luckylibrary/controllers/HomeController.java
+++ src/main/java/com/musaugurlu/luckylibrary/controllers/HomeController.java
@@ -5,16 +5,16 @@
 
 package com.musaugurlu.luckylibrary.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.musaugurlu.luckylibrary.models.Book;
 import com.musaugurlu.luckylibrary.services.BookService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class HomeController {
